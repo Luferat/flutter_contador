e47b4b2aaeb2009582e75b7f9a221941fabde624
André Luferat <contato@luferat.net>
2024-03-01 17:05:27 -0300

    Erro na Imagem

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      title: "Contador de Gente",
-      // Cria uma coluna na home
-      // O conteúdo da coluna é centralizado horizontalmente
-      home: Column(
+    title: "Contador de Gente",
+    home: Stack(
+      children: [
+        Image.asset(
+          "assets/fundo.jpg",
+          fit: BoxFit.cover,
+          height: double.infinity,
+          width: double.infinity,
+        ),
+        Column(
         // Alinha o conteúdo da coluna no meio vertical
         mainAxisAlignment: MainAxisAlignment.center,
         // Widgets da coluna
@@ -22,23 +28,29 @@ void main() {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "+1",
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.blue,
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "+1",
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.blue,
+                    ),
                   ),
                 ),
               ),
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "-1",
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.blue,
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "-1",
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.blue,
+                    ),
                   ),
                 ),
               ),
@@ -54,5 +66,8 @@ void main() {
             ),
           )
         ],
-      )));
+      )
+      ],
+    ),
+  ));
 }
