f7478b9ab80dd592bd4584a4860b3617fdd01944
André Luferat <contato@luferat.net>
2024-03-01 15:55:01 -0300

    Aula 02

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
       title: "Contador de Gente",
-      home: Container(
-        color: Colors.yellow,
+      // Cria uma coluna na home
+      // O conteúdo da coluna é centralizado horizontalmente
+      home: Column(
+        // Alinha o conteúdo da coluna no meio vertical
+        mainAxisAlignment: MainAxisAlignment.center,
+        // Widgets da coluna
+        children: [
+          // Primeira linha: um texto
+          Text(
+            "Pessoas: 0",
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+              // backgroundColor: Colors.yellow,
+            ),
+          ),
+          // Segunda linha: outro texto
+          Text(
+            "Pode entrar",
+            style: TextStyle(
+              color: Colors.blue,
+              fontStyle: FontStyle.italic,
+              fontSize: 30.0,
+            ),
+          )
+        ],
       )));
 }
