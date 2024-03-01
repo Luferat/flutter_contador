b05d96e28cd6824f5748674b0e8763e9e3369d66
André Luferat <contato@luferat.net>
2024-03-01 17:34:44 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     title: "Contador de Gente",
-    home: Stack(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _pessoas = 0;
+  String _msgText = "Pode entrar!";
+
+  void _changePessoas(int numero) {
+    setState(() {
+      _pessoas += numero;
+      if (_pessoas < 0) {
+        _msgText = "Abaixo de 0!";
+      } else if (_pessoas <= 10) {
+        _msgText = "Pode entrar!";
+      } else {
+        _msgText = "Lotado!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       children: [
         Image.asset(
           "assets/fundo.jpg",
@@ -12,62 +42,66 @@ void main() {
           width: double.infinity,
         ),
         Column(
-        // Alinha o conteúdo da coluna no meio vertical
-        mainAxisAlignment: MainAxisAlignment.center,
-        // Widgets da coluna
-        children: [
-          // Primeira linha: um texto
-          const Text(
-            "Pessoas: 0",
-            style: TextStyle(
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
-              // backgroundColor: Colors.yellow,
+          // Alinha o conteúdo da coluna no meio vertical
+          mainAxisAlignment: MainAxisAlignment.center,
+          // Widgets da coluna
+          children: [
+            // Primeira linha: um texto
+            Text(
+              "Pessoas: $_pessoas",
+              style: const TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+                // backgroundColor: Colors.yellow,
+              ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "+1",
-                    style: TextStyle(
-                      fontSize: 40,
-                      color: Colors.blue,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextButton(
+                    onPressed: () {
+                      _changePessoas(1);
+                    },
+                    child: const Text(
+                      "+1",
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.blue,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "-1",
-                    style: TextStyle(
-                      fontSize: 40,
-                      color: Colors.blue,
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextButton(
+                    onPressed: () {
+                      _changePessoas(-1);
+                    },
+                    child: const Text(
+                      "-1",
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.blue,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          // Segunda linha: outro texto
-          const Text(
-            "Pode entrar",
-            style: TextStyle(
-              color: Colors.blue,
-              fontStyle: FontStyle.italic,
-              fontSize: 30.0,
+              ],
             ),
-          )
-        ],
-      )
+            // Segunda linha: outro texto
+            Text(
+              _msgText,
+              style: const TextStyle(
+                color: Colors.blue,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0,
+              ),
+            )
+          ],
+        )
       ],
-    ),
-  ));
+    );
+  }
 }
